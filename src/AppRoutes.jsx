d42076954212a0fabdbee574288e92008f59d06c
Snap-sec <support@snapsec.co>
2026-06-15 16:00:57 +0530

    Adding bLog Redirector

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import HomePage from './pages/home-page/home-pages.jsx';
 import ModuleViewPage from './pages/ModuleViewPage/index.jsx';
 import DiscoveryPage from './pages/DiscoveryPage/index.jsx';
@@ -8,6 +9,23 @@ import AboutUsPage from './pages/AboutUsPage/index.jsx';
 import OurWorkPage from './pages/OurWorkPage/index.jsx';
 import ContactUsPage from './pages/ContactUsPage/index.jsx';
 import NotFoundPage from './pages/NotFoundPage/index.jsx';
+
+const BlogRedirect = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.location.replace('https://blog.snapsec.co/');
+  }, []);
+
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-white font-sans text-black">
+      <div className="flex flex-col items-center gap-[12px]">
+        <div className="h-[24px] w-[24px] animate-spin rounded-full border-2 border-gray-200 border-t-black" />
+        <p className="text-[14px] font-medium tracking-tight">Redirecting to Snapsec Blog...</p>
+      </div>
+    </div>
+  );
+};
 
 export default function AppRoutes() {
   return (
@@ -24,6 +42,8 @@ export default function AppRoutes() {
       <Route path="/about-us" element={<AboutUsPage />} />
       <Route path="/our-work" element={<OurWorkPage />} />
       <Route path="/contact-us" element={<ContactUsPage />} />
+      <Route path="/blog/*" element={<BlogRedirect />} />
+      <Route path="/blog" element={<BlogRedirect />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
